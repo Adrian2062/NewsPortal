a100ebb62dc4e2a1f8360ca75c8de1b865a61219
Adrian2062 <adrianpoudyal@gmail.com>
2025-11-21 16:22:11 +0545

    Created templates

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("post-list/", views.PostListView.as_view(), name="post-list"),
+    path("post-detail/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
 ]
