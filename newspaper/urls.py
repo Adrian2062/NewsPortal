923d6c403f202a48a403d3139832302e9efdf4ce
Adrian2062 <adrianpoudyal@gmail.com>
2025-11-21 15:34:08 +0545

    Initial commit on list.html

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import HomeView
+from . import views
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
+    path("post-list/", views.PostListView.as_view(), name="post-list"),
 ]
