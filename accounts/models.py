126b3d8c5b5f15c8b6c48ec58a0de2e178af1038
Adrian2062 <adrianpoudyal@gmail.com>
2025-11-26 16:11:40 +0545

    Created login, register page

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+    featured_image = models.ImageField(upload_to='posts/')
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.views import View
+from .forms import RegistrationForm
 
-# Create your views here.
+class RegistrationView(View):
+    def get(self, request):
+        form = RegistrationForm()
+        return render(request, "registration/register.html", {"form": form})
+
+    def post(self, request):
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # login(request, user) optional
+            return redirect("accounts:login")
+        return render(request, "registration/register.html", {"form": form})
